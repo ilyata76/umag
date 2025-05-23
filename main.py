da86fe9bd8f30d1587bcc232f8e3938c3d7887a6
ilyata76 <ilyata76@yandex.ru>
2025-05-23 20:57:41 +0300

    wip

--- main.py
+++ main.py
@@ -63,17 +63,12 @@ numpy_geometry = None and NumpyGeometryManager.load_geometry(f"{path_dir}/../ato
 if numpy_geometry is None or not numpy_geometry.any():  # type: ignore
     numpy_geometry = NumpyGeometryManager.generate_hcp_monomaterial_parallelepiped(
         lattice_constant=LatticeConstant(nano(0.2507), nano(0.408)),
-        size=XYZ(nano(3), nano(3), nano(3)),
+        size=XYZ(nano(1), nano(1), nano(1)),
         material_number=mat_lib["Co"].get_number(),
-        initial_direction=XYZ(-1, 0, 0),
+        initial_direction=XYZ(1, 0, 0),
         base_shift=None,
     )
     NumpyGeometryManager.save_geometry(f"{path_dir}/INITIAL", numpy_geometry)
-
-# numpy_geometry = np.array([
-#     [0, 0, 0, 0, 1, 0, mat_lib["Co"].get_number()],
-#     [nano(0.2507), 0, 0, 0, 1, 0, mat_lib["Co"].get_number()]]
-# )
 
 material_registry = MaterialRegistry({MaterialEnum.COBALT.value: mat_lib["Co"]})
 
@@ -81,40 +76,45 @@ geometry = Geometry(numpy_geometry, material_registry, macrocell_size=nano(1.01)
 
 solver = LLGSolver(strategy=SolverStrategy.HEUN)
 
-interaction_registry = InteractionRegistry(
-    {
-        InteractionEnum.EXCHANGE.value: ExchangeInteraction(cutoff_radius=nano(0.3)),
-        InteractionEnum.DEMAGNETIZATION.value: DemagnetizationInteraction(
-            cutoff_radius=nano(20), strategy="macrocells"
-        ),
-        # InteractionEnum.ANISOTROPY.value: AnisotropyInteraction(),
-        # InteractionEnum.EXTERNAL.value: ExternalInteraction(0.5, 0.05, 0.0),
-        InteractionEnum.THERMAL.value: ThermalInteraction(
-            300, femto(1)
-        )
-    }
-)
-
-simulation = Simulation(
-    geometry=geometry,
-    solver=solver,
-    material_registry=material_registry,
-    interaction_registry=interaction_registry,
-    dt=femto(1),
-)
-
-steps, save_every_step, update_macrocells_every_step = 1000000, 100, 10
-
-for i in range(steps):
-    simulation.simulate_one_step(
-        save_step=(i % save_every_step == 0), update_macrocells=(i % update_macrocells_every_step == 0)
+interactions = {
+    InteractionEnum.EXCHANGE.value: ExchangeInteraction(cutoff_radius=nano(0.5)),
+    InteractionEnum.THERMAL.value: ThermalInteraction(
+        100, femto(1)
     )
-    if i % save_every_step == 0:
-        step_data = simulation.get_steps()[-1]
-        save_data(
-            get_vvis_data(step_data),
-            f"{path_dir}/sconfiguration-{i:08d}.vvis"
+}
+
+for temperature in range(100, 1700, 100):
+    interactions[InteractionEnum.THERMAL.value] = ThermalInteraction(temperature, femto(1))
+    interaction_registry = InteractionRegistry(interactions)
+
+    simulation = Simulation(
+        geometry=geometry,
+        solver=solver,
+        material_registry=material_registry,
+        interaction_registry=interaction_registry,
+        dt=femto(1),
+    )
+
+    steps, save_every_step, update_macrocells_every_step = 50000, 5000, 1000
+
+    makedirs(f"{path_dir}/{temperature}", exist_ok=True)
+    step_data = simulation.get_steps()[-1]
+    save_data(get_vvis_data(step_data), f"{path_dir}/{temperature}/sconfiguration-{0:08d}.vvis")
+    NumpyGeometryManager.save_geometry(f"{path_dir}/{temperature}/geometry-{0:08d}", step_data.geometry.as_numpy())
+    save_data(get_shot_data(step_data, material_registry, interaction_registry), f"{path_dir}/{temperature}/stepdata-{0:08d}.shot")
+
+    for i in range(1, steps + 1):
+        simulation.simulate_one_step(
+            save_step=(i % save_every_step == 0), update_macrocells=(i % update_macrocells_every_step == 0)
         )
-        NumpyGeometryManager.save_geometry(f"{path_dir}/geometry-{i:08d}", step_data.geometry.as_numpy())
-        save_data(get_shot_data(step_data, material_registry, interaction_registry),
-                  f"{path_dir}/stepdata-{i:08d}.shot")
+        if i % save_every_step == 0:
+            step_data = simulation.get_steps()[-1]
+            save_data(
+                get_vvis_data(step_data),
+                f"{path_dir}/{temperature}/sconfiguration-{i:08d}.vvis"
+            )
+            NumpyGeometryManager.save_geometry(f"{path_dir}/{temperature}/geometry-{i:08d}", step_data.geometry.as_numpy())
+            save_data(get_shot_data(step_data, material_registry, interaction_registry), f"{path_dir}/{temperature}/stepdata-{i:08d}.shot")
+
+    last_step_geometry = simulation.get_steps()[-1].geometry.as_numpy()
+    geometry = Geometry(last_step_geometry, material_registry, macrocell_size=nano(1.01))
